2e2a22e3792d9c28a41b7db0dca4e70681068059
Astik-Joshi <astikj0shi48@gmail.com>
2025-07-10 14:47:04 +0530

    Everything fine

--- frontend/src/pages/CallPage.jsx
+++ frontend/src/pages/CallPage.jsx
@@ -16,7 +16,7 @@ import {
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import toast from "react-hot-toast";
 
-const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
+const STREAM_API_KEY = import.meta.env.VITE_STEAM_API_KEY;
 
 const CallPage = () => {
   const { id: callId } = useParams();
